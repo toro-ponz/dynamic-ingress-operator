aad7b3c3e0eddf6b4e4b3b6657dc0689aa6d4fd8
toro_ponz <toro-ponz@users.noreply.github.com>
2023-12-13 17:35:40 +0900

    Implement creating and deleting ingress

--- api/v1beta1/dynamicingress_types.go
+++ api/v1beta1/dynamicingress_types.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1beta1
 
 import (
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -28,18 +29,53 @@ type DynamicIngressSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of DynamicIngress. Edit dynamicingress_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Target         DynamicIngressTarget                 `json:"target"`
+	PassiveIngress *DynamicIngressTargetIngressTemplate `json:"passiveIngress,omitempty"`
+	ActiveIngress  *DynamicIngressTargetIngressTemplate `json:"activeIngress,omitempty"`
 }
 
 // DynamicIngressStatus defines the observed state of DynamicIngress
 type DynamicIngressStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Value     DynamicIngressStatusValue     `json:"value,omitempty"`
+	Condition DynamicIngressStatusCondition `json:"condition,omitempty"`
+}
+
+type DynamicIngressStatusValue string
+
+const (
+	DynamicIngressNotReady  = DynamicIngressStatusValue("NotReady")
+	DynamicIngressHealthy   = DynamicIngressStatusValue("Healthy")
+	DynamicIngressUnhealthy = DynamicIngressStatusValue("Unhealthy")
+)
+
+type DynamicIngressStatusCondition string
+
+const (
+	False = DynamicIngressStatusCondition("False")
+	True  = DynamicIngressStatusCondition("True")
+	Error = DynamicIngressStatusCondition("Error")
+)
+
+type DynamicIngressTargetIngressTemplate struct {
+	Metadata DynamicIngressTargetIngressTemplateMetadata `json:"metadata"`
+	Spec     networkingv1.IngressSpec                    `json:"spec"`
+}
+
+type DynamicIngressTargetIngressTemplateMetadata struct {
+	Annotations map[string]string `json:"annotations,omitempty"`
+	Labels      map[string]string `json:"labels,omitempty"`
+}
+
+type DynamicIngressTarget struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
 }
 
 //+kubebuilder:object:root=true
 //+kubebuilder:subresource:status
+//+kubebuilder:printcolumn:name="STATUS",type="string",JSONPath=".status.value"
 
 // DynamicIngress is the Schema for the dynamicingresses API
 type DynamicIngress struct {
--- internal/controller/dynamicingress_controller.go
+++ internal/controller/dynamicingress_controller.go
@@ -18,13 +18,23 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"time"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	ingressv1beta1 "github.com/toro-ponz/dynamic-ingress-operator/api/v1beta1"
+	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+const (
+	DEBUG = 1
 )
 
 // DynamicIngressReconciler reconciles a DynamicIngress object
@@ -36,6 +46,7 @@ type DynamicIngressReconciler struct {
 //+kubebuilder:rbac:groups=ingress.toroponz.io,resources=dynamicingresses,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ingress.toroponz.io,resources=dynamicingresses/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=ingress.toroponz.io,resources=dynamicingresses/finalizers,verbs=update
+//+kubebuilder:rbac:groups=networking.k8s.io,resources=ingresses,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,9 +58,27 @@ type DynamicIngressReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *DynamicIngressReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logger := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	var dynamicIngress ingressv1beta1.DynamicIngress
+	err := r.Get(ctx, req.NamespacedName, &dynamicIngress)
+	if apierrors.IsNotFound(err) {
+		return ctrl.Result{}, nil
+	}
+
+	if err != nil {
+		logger.Error(err, "unable to get DynamicIngress", "name", req.NamespacedName)
+		return ctrl.Result{}, err
+	}
+
+	if !dynamicIngress.ObjectMeta.DeletionTimestamp.IsZero() {
+		return ctrl.Result{}, nil
+	}
+
+	err = r.reconcileIngress(ctx, dynamicIngress)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
@@ -58,5 +87,110 @@ func (r *DynamicIngressReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 func (r *DynamicIngressReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&ingressv1beta1.DynamicIngress{}).
+		Owns(&networkingv1.Ingress{}).
+		// TODO: watch
 		Complete(r)
+}
+
+func (r *DynamicIngressReconciler) reconcileIngress(ctx context.Context, dynamicIngress ingressv1beta1.DynamicIngress) error {
+	logger := log.FromContext(ctx)
+
+	target := dynamicIngress.Spec.Target
+
+	isActive, err := r.checkConditions(ctx)
+	if err != nil {
+		return err
+	}
+
+	if isActive {
+		err = r.applyIngress(ctx, target, dynamicIngress.Spec.ActiveIngress)
+		if err != nil {
+			logger.Error(err, fmt.Sprintf("unable to apply active ingress (name=%s, namespace=%s)", target.Name, target.Name))
+			return err
+		}
+
+		logger.Info(fmt.Sprintf("succeeded apply active ingress (name=%s, namespace=%s)", target.Name, target.Name))
+	} else {
+		err = r.applyIngress(ctx, target, dynamicIngress.Spec.PassiveIngress)
+		if err != nil {
+			logger.Error(err, fmt.Sprintf("unable to apply passive ingress (name=%s, namespace=%s)", target.Name, target.Name))
+			return err
+		}
+
+		logger.Info(fmt.Sprintf("succeeded apply passive ingress (name=%s, namespace=%s)", target.Name, target.Name))
+	}
+
+	return nil
+}
+
+func (r *DynamicIngressReconciler) applyIngress(
+	ctx context.Context,
+	target ingressv1beta1.DynamicIngressTarget,
+	ingressTemplate *ingressv1beta1.DynamicIngressTargetIngressTemplate,
+) error {
+	logger := log.FromContext(ctx)
+
+	ingress := &networkingv1.Ingress{}
+	ingress.SetName(target.Name)
+	ingress.SetNamespace(target.Namespace)
+
+	if ingressTemplate != nil {
+		logger.V(DEBUG).Info(fmt.Sprintf("start createOrUpdate ingress (name=%s, namespace=%s)", ingress.Name, ingress.Namespace))
+
+		op, err := ctrl.CreateOrUpdate(ctx, r.Client, ingress, func() error {
+			ingress.Spec = ingressTemplate.Spec
+			ingress.Annotations = ingressTemplate.Metadata.Annotations
+			ingress.Labels = ingressTemplate.Metadata.Labels
+			return nil
+		})
+
+		if err != nil {
+			return err
+		}
+
+		if op != controllerutil.OperationResultNone {
+			logger.Info(fmt.Sprintf("ingress changes applied (name=%s, namespace=%s)", ingress.Name, ingress.Namespace))
+			return nil
+		}
+
+		logger.V(DEBUG).Info(fmt.Sprintf("end createOrUpdate ingress (name=%s, namespace=%s)", ingress.Name, ingress.Namespace))
+	} else {
+		logger.V(DEBUG).Info(fmt.Sprintf("start delete ingress (name=%s, namespace=%s)", ingress.Name, ingress.Namespace))
+
+		err := r.Get(ctx, client.ObjectKey{Namespace: ingress.Namespace, Name: ingress.Name}, ingress)
+		if err != nil && apierrors.IsNotFound(err) {
+			logger.V(DEBUG).Info(fmt.Sprintf("ingress not found (name=%s, namespace=%s)", ingress.Name, ingress.Namespace))
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+
+		uid := ingress.GetUID()
+		resourceVersion := ingress.GetResourceVersion()
+		err = r.Delete(ctx, ingress, &client.DeleteOptions{
+			Preconditions: &metav1.Preconditions{
+				UID:             &uid,
+				ResourceVersion: &resourceVersion,
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		logger.Info(fmt.Sprintf("ingress deleted (name=%s, namespace=%s)", ingress.Name, ingress.Namespace))
+		logger.V(DEBUG).Info(fmt.Sprintf("end createOrUpdate ingress (name=%s, namespace=%s)", ingress.Name, ingress.Namespace))
+	}
+
+	return nil
+}
+
+func (r *DynamicIngressReconciler) checkConditions(ctx context.Context) (bool, error) {
+	// TODO: check watcher status
+	_, minutes, _ := time.Now().Clock()
+	if minutes%2 == 0 {
+		return true, nil
+	}
+
+	return false, nil
 }
