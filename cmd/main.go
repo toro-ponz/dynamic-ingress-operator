11513c5691f537c2d5789be8f6386f985bb0e177
toro_ponz <toro-ponz@users.noreply.github.com>
2023-12-17 18:10:57 +0900

    Add dynamic ingress state resource
    kubebuilder create api --group ingress --version v1beta1 --kind DynamicIngressState

--- cmd/main.go
+++ cmd/main.go
@@ -96,6 +96,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DynamicIngress")
 		os.Exit(1)
 	}
+	if err = (&controller.DynamicIngressStateReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DynamicIngressState")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
